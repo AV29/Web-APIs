2c0ec6b69c88067c83d95b542b613be0674fb8b4
Anton Vlasik <antonvlasik@coherentsolutions.com>
2018-03-30 00:15:33 +0300

    Transformed script to react app

--- src/components/speech/speech-synthesis/SpeechSynthesis.jsx
+++ src/components/speech/speech-synthesis/SpeechSynthesis.jsx
@@ -1,127 +1,119 @@
 import React, {Component} from 'react';
 import routesConfiguration from '../../../routing/routesConfiguration'
+import classNames from 'classnames';
 import * as styles from '../../../styles/global.less';
 import * as ownStyles from './SpeechSynthesis.less';
 
 const {speechSynthesis} = routesConfiguration;
 
 class SpeechSynthesis extends Component {
+  constructor(props) {
+    super(props);
+    console.log('Constructor');
+    this.voices = [];
+    this.synth = window.speechSynthesis;
 
-  componentDidMount() {
-    const synth = window.speechSynthesis;
+    this.state = {
+      pitch: 1,
+      rate: 1,
+      text: '',
+      voice: ''
+    };
 
-    const inputForm = document.querySelector('form');
-    const inputTxt = document.querySelector('.txt');
-    const voiceSelect = document.querySelector('select');
+    this.getVoices();
+    speechSynthesis.onvoiceschanged = this.getVoices;
 
-    const pitch = document.querySelector('#pitch');
-    const pitchValue = document.querySelector('.pitch-value');
-    const rate = document.querySelector('#rate');
-    const rateValue = document.querySelector('.rate-value');
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+    this.speak = this.speak.bind(this);
+  }
 
-    let voices = [];
+  getVoices() {
+    console.log('Get voices');
+    this.voices = this.synth.getVoices();
+  }
 
-    function populateVoiceList() {
-      voices = synth.getVoices();
-      const selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
-      voiceSelect.innerHTML = '';
-      for(let i = 0; i < voices.length ; i++) {
-        const option = document.createElement('option');
-        option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
-
-        if(voices[i].default) {
-          option.textContent += ' -- DEFAULT';
+  speak() {
+    if (this.synth.speaking) {
+      console.error('speechSynthesis.speaking');
+      return;
+    }
+    if (this.state.text !== '') {
+      const utterThis = new SpeechSynthesisUtterance(this.state.text);
+      for(let i = 0; i < this.voices.length ; i++) {
+        if(this.voices[i].name === this.state.voice) {
+          utterThis.voice = this.voices[i];
         }
-
-        option.setAttribute('data-lang', voices[i].lang);
-        option.setAttribute('data-name', voices[i].name);
-        voiceSelect.appendChild(option);
       }
-      voiceSelect.selectedIndex = selectedIndex;
+
+      utterThis.pitch = this.state.pitch;
+      utterThis.rate = this.state.rate;
+      this.synth.speak(utterThis);
     }
+  }
 
-    populateVoiceList();
-    if (speechSynthesis.onvoiceschanged !== undefined) {
-      speechSynthesis.onvoiceschanged = populateVoiceList;
-    }
+  handleChange({target: {value, name}}) {
+    this.setState({[name]: value});
+  }
 
-    function speak(){
-      if (synth.speaking) {
-        console.error('speechSynthesis.speaking');
-        return;
-      }
-      if (inputTxt.value !== '') {
-        const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
-        utterThis.onend = function (event) {
-          console.log('SpeechSynthesisUtterance.onend');
-        };
-        utterThis.onerror = function (event) {
-          console.error('SpeechSynthesisUtterance.onerror');
-        };
-        const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
-        for(let i = 0; i < voices.length ; i++) {
-          if(voices[i].name === selectedOption) {
-            utterThis.voice = voices[i];
-          }
-        }
-        utterThis.pitch = pitch.value;
-        utterThis.rate = rate.value;
-        synth.speak(utterThis);
-      }
-    }
-
-    inputForm.onsubmit = function(event) {
-      event.preventDefault();
-
-      speak();
-
-      inputTxt.blur();
-    };
-
-    pitch.onchange = function() {
-      pitchValue.textContent = pitch.value;
-    };
-
-    rate.onchange = function() {
-      rateValue.textContent = rate.value;
-    };
-
-    voiceSelect.onchange = function(){
-      speak();
-    }
+  handleSelectChange({target: {value}}) {
+    this.setState(state => ({voice: value}), this.speak);
   }
 
   render() {
+    console.log('Render');
     return (
-      <div className={`${styles.pageWrapper} ${ownStyles.speechSynthesisWrapper}`}>
+      <div className={classNames(styles.pageWrapper, ownStyles.speechSynthesisWrapper)}>
         <h2>{speechSynthesis.title}</h2>
-        <p>
-          Enter some text in the input below and press return or the "play" button to hear it. change voices using the
-          dropdown menu.
-        </p>
-
-        <form>
-          <input type="text" className={ownStyles.txt}/>
-          <div>
-            <label htmlFor="rate">Rate</label><input type="range" min="0.5" max="2" value="1" step="0.1" id="rate"/>
-            <div className={ownStyles.rateValue}>1</div>
-            <div className={ownStyles.clearfix}/>
-          </div>
-          <div>
-            <label htmlFor="pitch">Pitch</label><input type="range" min="0" max="2" value="1" step="0.1" id="pitch"/>
-            <div className={ownStyles.pitchValue}>1</div>
-            <div className={ownStyles.clearfix}/>
-          </div>
-          <select>
-
-          </select>
-          <div className={ownStyles.controls}>
-            <button id="play" type="submit">Play</button>
-          </div>
-        </form>
+        <input
+          type="text"
+          name="text"
+          className={ownStyles.txt}
+          onChange={this.handleChange}
+        />
+        <div>
+          <label htmlFor="rate">Rate</label>
+          <input
+            onChange={this.handleChange}
+            name="rate"
+            type="range"
+            min="0.5"
+            max="2"
+            value={this.state.rate}
+            step="0.1"
+          />
+          <div className={ownStyles.rateValue}>{this.state.rate}</div>
+        </div>
+        <div>
+          <label htmlFor="pitch">Pitch</label>
+          <input
+            onChange={this.handleChange}
+            name="pitch"
+            type="range"
+            min="0"
+            max="2"
+            value={this.state.pitch}
+            step="0.1"
+          />
+          <div className={ownStyles.pitchValue}>{this.state.pitch}</div>
+        </div>
+        <select
+          name="voice"
+          onChange={this.handleSelectChange}
+          value={this.state.voice}
+        >
+          {
+            this.voices.map((voice, index) =>
+              <option key={index} value={voice.name}>{`${voice.name} ${voice.lang}`}</option>
+            )
+          }
+        </select>
+        <div className={ownStyles.controls}>
+          <button onClick={this.speak}>Play</button>
+        </div>
       </div>
     );
   }
-};
+}
 
 export default SpeechSynthesis;
