06ae88700280d9f1cabd494ecfe31e5ae4abb107
Anton Vlasik <snumber29@gmail.com>
2019-05-01 21:10:15 +0300

    added vidoe to network nformation

--- src/components/network-information/NetworkInformation.jsx
+++ src/components/network-information/NetworkInformation.jsx
@@ -78,12 +78,7 @@ class NetworkInformation extends Component {
           {networkInfo.title}
         </h3>
         <div id="currentConnectionInfo"/>
-        <video id="video" muted loop width="1480" height="360">
-          <source
-            src="https://r2---sn-4g5ednsl.googlevideo.com/videoplayback?id=o-AJDCyJDjAYjcK8azjfXFC6r1dpc9m7k7nrwqibPKk5hM&itag=18&source=youtube&requiressl=yes&pl=19&ei=j8vFXK_MNeGoz7sPzMeo0As&mime=video%2Fmp4&gir=yes&clen=10264094&ratebypass=yes&dur=247.338&lmt=1443298675445856&fvip=2&c=WEB&ip=59.91.123.94&ipbits=0&expire=1556488176&sparams=clen,dur,ei,expire,gir,id,ip,ipbits,ipbypass,itag,lmt,mime,mip,mm,mn,ms,mv,pl,ratebypass,requiressl,source&key=cms1&signature=6E1EB422F000FD6A4C61EC5D195C2A7646832BE3.780324EACA257DE5642BF797934C211F83D8310F&video_id=f56lELufeQE&title=Enigma+Return+to+Innocence+Music+Video+Official&rm=sn-qxay7d&req_id=55c397c701b8a3ee&ipbypass=yes&mip=46.56.230.230&cm2rm=sn-ivhxoxufvg3g0-hn9e7l,sn-gvnuxaxjvh-n8vk7d&redirect_counter=3&cms_redirect=yes&mm=30&mn=sn-4g5ednsl&ms=nxu&mt=1556466517&mv=m"
-            type="video/mp4"
-            media="all and (max-width:680px)"
-          />
+        <video id="video" muted loop width="1480" height="360" src={"assets/videoplayback.mp4"}>
         </video>
         <div id="videoInfo"/>
         <div id="changedConnectionInfo"/>
